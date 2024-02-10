b9f015db9871be82fe92c8748df17a25a9a3ee82
letbrasil <le.brasileiro@gmail.com>
2024-02-10 15:30:04 -0300

    Department CRUD [scaffolding]

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,8 @@
 ﻿namespace SalesWebMvc.Models
 {
-	public class Department
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 }
